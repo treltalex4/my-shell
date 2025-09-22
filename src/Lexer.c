4d3656719b5c60934c99f7a023607f5795ca2386
Wernt <shelalex06@gmail.com>
2025-09-23 01:10:01 +0500

    Update Lexer.c

--- src/Lexer.c
+++ src/Lexer.c
@@ -2,6 +2,7 @@
 
 #include "Lexer.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 static Token lexer_extract(Lexer *lexer);
@@ -9,6 +10,10 @@ static Token lexer_extract_basic(Lexer *lexer);
 static Token lexer_extract_pipe(Lexer *lexer);
 static Token lexer_extract_redir(Lexer *lexer);
 static Token lexer_extract_control(Lexer *lexer);
+
+static Token make_error_token(size_t pos, const char *message);
+static Token make_simple_token(TokenType type, size_t pos);
+static void skip_whitespace_and_comments(Lexer *lexer);
 
 void lexer_init(Lexer *lexer, const char *input){
     if(!lexer){
@@ -46,6 +51,44 @@ void lexer_destroy(Lexer *lexer){
     return;
 }
 
-Token lexer_tokenize(Lexer *lexer){
-    
+Token lexer_tokenize(Lexer *lexer)
+{
+    if (!lexer) {
+        perror("lexer_tokenize: null ptr");
+        return make_error_token(0, "lexer_tokenize: null ptr");
+    }
+
+    if (!lexer->input) {
+        perror("lexer_tokenize: null input");
+        return make_error_token(lexer->pos, "lexer_tokenize: null input");
+    }
+
+    return lexer_extract(lexer);
+}
+
+static Token lexer_extract(Lexer *lexer){
+
+}
+
+static Token make_error_token(size_t pos, const char *message){
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.quote = QUOTE_NONE;
+    token.pos = pos;
+
+    if (message) {
+        size_t len = strlen(message) + 1;
+        char *copy = malloc(len);
+        if (copy) {
+            memcpy(copy, message, len);
+            token.text = copy;
+        } else {
+            perror("lexer: failed to allocate error message");
+            token.text = NULL;
+        }
+    } else {
+        token.text = NULL;
+    }
+
+    return token;
 }
